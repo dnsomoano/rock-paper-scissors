dfaba44608964c0e3bf10addf0b9ed482b6d8545
Daniel N Somoano <dnsomoano@gmail.com>
2018-08-27 05:09:44 -0400

    .

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Rock Paper Scissors
+# Rock-Paper-Scissors #
+Create a simple Rock-Paper-Scissors game
+
+## Objectives ##
+* Work with JavaScript and the DOM
+* Practice if/then logic
+PRO-TIP
+Do not worry about CSS for explorer mode, only do as HTML to show the functionality
+
+Explorer Mode
+- [X] As a user I should be able to see three buttons for each player
+- [X] as a user, I should be able to pick an option for each player
+- [X] As a user,after each player makes their selection, I should be told who won and the game should be over
+Adventure Mode
+- [ ] Create a vs computer mode, that picks a selection
+- [ ] Modify your vs computer mode to follow a specific strategy (your choice)
+- [ ] Make your computer AI unbeatable
+- [ ] Give the user a choice to player against the various computer levels or another player
+- [ ] Have fun with the CSS by adding colors, animations and effects to make the game more enjoyable
+Epic Mode
+- [ ] Your game is currently only 1 vs 1, add the ability to add more computer or users players to the game
--- public/main.js
+++ public/main.js
@@ -6,17 +6,98 @@
 
 //
 // Player 1 choice
-const playerOneSelection = () => {
-  const playerOneChoice = document.querySelector('.player-one-button').textContent;
-  console.log(playerOneChoice);
-}
+const playerOneRockSelection = () => {
+  const playerOneRock = document.querySelector(".player-one-choice")
+    .textContent;
+  console.log("player one rock");
+};
 
+const playerOnePaperSelection = () => {
+  const playerOneRock = document.querySelector(".player-one-choice")
+    .textContent;
+  console.log("player one paper");
+};
+
+const playerOneScissorsSelection = () => {
+  const playerOneRock = document.querySelector(".player-one-choice")
+    .textContent;
+  console.log("player one scissors");
+};
+
+// Player 2 choice
+const playerTwoRockSelection = () => {
+  const playerOneRock = document.querySelector(".player-two-choice")
+    .textContent;
+  console.log("player two rock");
+};
+
+const playerTwoPaperSelection = () => {
+  const playerOneRock = document.querySelector(".player-two-choice")
+    .textContent;
+  console.log("player two paper");
+};
+
+const playerTwoScissorsSelection = () => {
+  const playerOneRock = document.querySelector(".player-two-choice")
+    .textContent;
+  console.log("player two scissors");
+};
+// Assign data values to each button
 // Player 1
-document.querySelector('.player-one-button').addEventListener('click', playerOneSelection)
-// document.querySelector("playerOnePaper").addEventListener('click', playerOneSelection)
-// document.querySelector("playerOneScissors").addEventListener('click', playerOneSelection)
+const playerOneRock = document.querySelector(".player-one-rock-button")
+  .textContent;
+const playerOnePaper = document.querySelector(".player-one-paper-button")
+  .textContent;
+const playerOneScissors = document.querySelector(".player-one-scissors-button")
+  .textContent;
 
 // // Player 2
-// document.querySelector("playerTwoRock").addEventListener('click', playerTwoSelection)
-// document.querySelector("playerTwoPaper").addEventListener('click', playerTwoSelection)
-// document.querySelector("playerTwoScissors").addEventListener('click', playerTwoSelection)
+const playerTwoRock = document.querySelector(".player-two-rock-button")
+  .textContent;
+const playerTwoPaper = document.querySelector(".player-two-paper-button")
+  .textContent;
+const playerTwoScissors = document.querySelector(".player-two-scissors-button")
+  .textContent;
+
+const playerOneSelection = () => {
+  // Conditions for Rock, Paper, Scissors
+  if (
+    (playerOneChoice == playerOneRock && playerTwoChoice == playerTwoPaper) ||
+    (playerOneChoice == playerOnePaper &&
+      playerTwoChoice == playerTwoScissors) ||
+    (playerOneChoice == playerOneScissors && playerTwoChoice == playerTwoRock)
+  ) {
+    console.log("Player 2 wins!");
+    if (
+      (playerOneChoice == playerOneRock &&
+        playerTwoChoice == playerTwoScissors) ||
+      (playerOnePaper && playerTwoChoice == playerTwoRock) ||
+      (playerOneChoice == playerOneScissors &&
+        (playerTwoChoice == playerOneChoice) == player)
+    ) {
+      console.log("Player 1 wins!");
+    } else console.log("Draw!");
+  }
+};
+
+// Player 1 events
+document
+  .querySelector(".player-one-rock-button")
+  .addEventListener("click", playerOneRockSelection);
+document
+  .querySelector(".player-one-paper-button")
+  .addEventListener("click", playerOnePaperSelection);
+document
+  .querySelector(".player-one-scissors-button")
+  .addEventListener("click", playerOneScissorsSelection);
+
+// // Player 2
+document
+  .querySelector(".player-two-rock-button")
+  .addEventListener("click", playerTwoRockSelection);
+document
+  .querySelector(".player-two-paper-button")
+  .addEventListener("click", playerTwoPaperSelection);
+document
+  .querySelector(".player-two-scissors-button")
+  .addEventListener("click", playerTwoScissorsSelection);
